4fcd1be1b56d16c0c375b91652625aee30ed6141
DanteLv3 <44603917+DanteLv3@users.noreply.github.com>
2018-11-05 11:25:33 -0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 var years_mort = 0;
 var rate_int = 0;
 
+
 // Aquí vemos el código que se encarga de tomar los avlores en vivo de los sliders
 
 var slider1 = document.getElementById("years_mort");
@@ -14,6 +15,8 @@ output1.innerHTML = slider1.value;
 slider1.oninput = function() {
     output1.innerHTML = this.value;
 	years_mort = parseFloat(this.value);
+	var percent = (years_mort * 100)/40;
+	document.getElementById("years_mort").style.background = "linear-gradient(to right, #1091cc " + percent + "%, #d8d8d8 " + percent + "%";
 }
 
 var slider2 = document.getElementById("rate_int");
@@ -21,6 +24,8 @@ var output2 = document.getElementById("rate_int_out");
 output2.innerHTML = slider2.value;  
 
 slider2.oninput = function() {
+	var percent = this.value;
+	document.getElementById("rate_int").style.background = "linear-gradient(to right, #1091cc " + percent + "%, #d8d8d8 " + percent + "%";
     output2.innerHTML = (this.value / 10);
 	rate_int = parseFloat(this.value) / 10;
 }
@@ -30,9 +35,9 @@ slider2.oninput = function() {
 
 function cambiaValor(){
 	output1.innerHTML = slider1.value;
-	years_mort = parseFloat(this.value);
+	years_mort = 20;
 	output2.innerHTML = (slider2.value / 10);
-	rate_int = parseFloat(this.value) / 10;
+	rate_int = (50/10)
 }
 
 //isNumber() verifica que los valores ingresados en los campos input sean numéricos
@@ -51,6 +56,10 @@ function isNumber(evt) {
 	
 	elem3 = document.getElementById("insurance_ammount");
 	elem3.className = "field";
+	
+	document.getElementById("mandTxt_1").style.display = "none";
+	document.getElementById("mandTxt_2").style.display = "none";
+	document.getElementById("mandTxt_3").style.display = "none";
 	
     evt = (evt) ? evt : window.event;
     var charCode = (evt.which) ? evt.which : evt.keyCode;
@@ -71,15 +80,30 @@ function calculate(){
 	
 	if(validate(loan_ammount, tax_ammount, insurance_ammount)){
 	    //Aquí se calcula el resultado
+		document.getElementById("btn_text").innerHTML = "RECALCULATE";
+		
 		var principle = ((rate_int / 100) / 12) * (loan_ammount / (1 - (Math.pow((1 + ((rate_int / 100)/12)),- (years_mort*12)))));
+		principle = Math.round(principle * 100) / 100;
 		document.getElementById("principle").innerHTML = "$ " + principle;
+		document.getElementById("principle").style.opacity = "1";
+		
 		var tax = tax_ammount/12;
+		tax = Math.round(tax * 100) / 100;
 		document.getElementById("tax").innerHTML = "$ " + tax;
+		document.getElementById("tax").style.opacity = "1";
+		
 		var insurance = insurance_ammount/12;
+		insurance = Math.round(insurance * 100) / 100;
 		document.getElementById("insurance").innerHTML = "$ " + insurance;
+		document.getElementById("insurance").style.opacity = "1";
+		
 		var result = principle + tax + insurance;
-		document.getElementById("result").innerHTML = "$ " + insurance;
+		result = Math.round(result * 100) / 100;
+		document.getElementById("result").innerHTML = "$ " + result;
+		document.getElementById("result").style.opacity = "1";
+		
 		var w = window.innerWidth;
+		
 		if(w <= 768){
 			document.getElementById("result-box").style.display = "block";
 			//console.log(document.getElementById("result-box").getBoundingClientRect());
@@ -102,11 +126,13 @@ function validate(num1, num2, num3){
 		val1 = false;
 		elem = document.getElementById("loan_ammount");
 		elem.className = "field-normal-shadow-alt";
+		document.getElementById("mandTxt_1").style.display = "block";
 	}else{
 		if(num1 === 0){
 			val1 = false;
 			elem = document.getElementById("loan_ammount");
 			elem.className = "field-normal-shadow-alt";
+			document.getElementById("mandTxt_1").style.display = "block";
 		}else{
 			val1 = true;
 		}
@@ -116,11 +142,13 @@ function validate(num1, num2, num3){
 		val2 = false;
 		elem = document.getElementById("tax_ammount");
 		elem.className = "field-alt";
+		document.getElementById("mandTxt_2").style.display = "block";
 	}else{
 		if(num2 === 0){
 			val2 = false;
 			elem = document.getElementById("tax_ammount");
 			elem.className = "field-alt";
+			document.getElementById("mandTxt_2").style.display = "block";
 		}else{
 			val2 = true;
 		}
@@ -130,11 +158,13 @@ function validate(num1, num2, num3){
 		val3 = false;
 		elem = document.getElementById("insurance_ammount");
 		elem.className = "field-alt";
+		document.getElementById("mandTxt_3").style.display = "block";
 	}else{
 		if(num3 === 0){
 			val3 = false;
 			elem = document.getElementById("insurance_ammount");
 			elem.className = "field-alt";
+			document.getElementById("mandTxt_3").style.display = "block";
 		}else{
 			val3 = true;
 		}
@@ -150,6 +180,7 @@ function validate(num1, num2, num3){
 //Función que anima los resultados cuando la calculadora está en modo móvil
 
 function animResults() {
+  window.scrollBy(0,1000);
   var elem = document.getElementById("result-box");   
   var pos = -225;
   var id = setInterval(frame, 2);
